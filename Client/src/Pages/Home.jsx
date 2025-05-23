064fce0159fcab7a076eafa1988507fa19b9217b
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-05-23 23:03:47 +0530

    feat: categories section added

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MainBanner from "../Components/MainBanner";
+import Categories from "../Components/Categories";
 
 const Home = () => {
   return (
     <div>
       <MainBanner />
+      <Categories />
     </div>
   );
 };
