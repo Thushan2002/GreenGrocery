6bb6ec50c4507cb7d1f336e6857743e384dbc664
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-05-25 21:03:08 +0530

    feat: responsiveness added

--- Client/src/Components/BestSeller.jsx
+++ Client/src/Components/BestSeller.jsx
@@ -7,8 +7,13 @@ const BestSeller = () => {
   return (
     <div className="mt-16">
       <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
-      <div>
-        <ProductCard product={products[0]} />
+      <div className="grid grid-cols-1 place-items-center sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2 mt-6">
+        {products
+          .filter((product) => product.inStock)
+          .slice(0, 5)
+          .map((product, index) => (
+            <ProductCard key={index} product={product} />
+          ))}
       </div>
     </div>
   );
