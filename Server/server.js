20b4b319ecbcd1c04f3adcb1c7a59a0914024aa6
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-06-01 09:30:03 +0530

    fix: mongodb connection fixed

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,15 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors"
+import connectDB from "./Configs/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express()
 
 const port = process.env.PORT || 4000
+await connectDB()
 
 const allowedOrigins = ['http://localhost:5173']
 
