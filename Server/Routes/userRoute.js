f4bd839a22da5af2a2525f194fec45080a17d8d5
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-06-02 20:53:48 +0530

    feat: user login function added

--- Server/Routes/userRoute.js
+++ Server/Routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { registerUser } from '../Controllers/UserController.js';
+import { login, registerUser } from '../Controllers/UserController.js';
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser)
+userRouter.post("/login", login)
 
 export default userRouter;
 
