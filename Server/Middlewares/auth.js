43fafea467693eb9ba6d3a7913a5d88b3c09a364
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-08-10 14:39:47 +0530

    fix: user authentication fixed

--- Server/Middlewares/auth.js
+++ Server/Middlewares/auth.js
@@ -2,20 +2,25 @@ import jwt from "jsonwebtoken";
 
 const authUser = async (req, res, next) => {
     const { token } = req.cookies;
+
     if (!token) {
         return res.status(401).json({ success: false, message: "Unauthorized 1" });
     }
+
     try {
         const tokenCode = jwt.verify(token, process.env.JWT_SECRET);
-        if (tokenCode.id) {
-            req.body.userId = tokenCode.id;
+
+        if (tokenCode.userId) {
+            req.user = tokenCode.userId;
         } else {
             return res.status(401).json({ success: false, message: "Unauthorized 2" });
         }
-        next()
+        next();
+
     } catch (error) {
+        console.error("JWT verify error:", error);
         return res.status(401).json({ success: false, message: "Unauthorized 3" });
     }
-}
+};
 
-export default authUser;
+export default authUser
